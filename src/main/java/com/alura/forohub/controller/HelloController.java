28bd6407e6dd6afd786033f0d8256c43cab6ace2
user-name-c <ivandgr_16@hotmail.com>
2024-06-22 15:26:38 -0600

    Creacion del proyecto con spring

---
+++ src/main/java/com/alura/forohub/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.alura.forohub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+
+    @GetMapping
+    public String helloWorld() {
+        return "Sin problemas en la configuracion inicial!";
+    }
+
+}
+
