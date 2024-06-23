ae84e2e1473bb71b6a82f2a9466a2996488cf83e
user-name-c <ivandgr_16@hotmail.com>
2024-06-22 21:05:38 -0600

    Corrige error en la creacion de tabla topicos

--- src/main/resources/db/migration/V1__create-table-topicos.sql
+++ src/main/resources/db/migration/V1__create-table-topicos.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Topicos (
+CREATE TABLE topicos (
     id BIGINT NOT NULL AUTO_INCREMENT,
     titulo VARCHAR(100) NOT NULL UNIQUE,
     mensaje VARCHAR(300) NOT NULL UNIQUE,
-    fechaCreacion VARCHAR(100) NOT NULL,
+    fecha_creacion VARCHAR(100) NOT NULL,
     status TINYINT,
     autor VARCHAR(100) NOT NULL,
     curso VARCHAR(100) NOT NULL,
