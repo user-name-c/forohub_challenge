522c392ae0dfefd795621aebbe736a57b54f3ae4
user-name-c <ivandgr_16@hotmail.com>
2024-06-25 11:06:42 -0600

    Ruta /topicos/id GET devuelve solo si el topico esta activo (no eliminados)

--- src/main/java/com/alura/forohub/controller/TopicoController.java
+++ src/main/java/com/alura/forohub/controller/TopicoController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -37,17 +38,21 @@ public class TopicoController {
 
     @GetMapping("/{id}")
     public ResponseEntity detallarTopico(@PathVariable Long id) {
-        var topico = repository.getReferenceById(id);
-        return ResponseEntity.ok(new ResponseTopicoDTO(topico));
+        if (repository.existsById(id) && repository.findActivoById(id)){
+            var topico = repository.getReferenceById(id);
+            return ResponseEntity.ok(new ResponseTopicoDTO(topico));
+        } return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe el tópico solicitado o fue borrado");
     }
 
     @DeleteMapping("/{id}")
     @Transactional
     public ResponseEntity eliminarTopico(@PathVariable Long id) {
-        //Delete logico
-        Topico topico = repository.getReferenceById(id);
-        topico.desactivarTopico();
-        return ResponseEntity.noContent().build();
+        if (repository.existsById(id) && repository.findActivoById(id)){
+            //Delete logico
+            Topico topico = repository.getReferenceById(id);
+            topico.desactivarTopico();
+            return ResponseEntity.noContent().build();
+        } return ResponseEntity.status(HttpStatus.NOT_FOUND).body("No existe el tópico solicitado o fue borrado");
     }
 
 }
--- src/main/java/com/alura/forohub/domain/topico/TopicoRepository.java
+++ src/main/java/com/alura/forohub/domain/topico/TopicoRepository.java
@@ -3,6 +3,7 @@ package com.alura.forohub.domain.topico;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,4 +11,11 @@ import java.util.Optional;
 @Repository
 public interface TopicoRepository extends JpaRepository <Topico, Long>{
     Page<Topico> findByActivoTrue(Pageable paginacion);
+
+    @Query("""
+            select t.activo
+            from topico t
+            where t.id=:idTopico
+            """)
+    Boolean findActivoById(Long idTopico);
 }
