1b4f1db2043e8a90dbf5ec60a026b4e28003abe2
user-name-c <ivandgr_16@hotmail.com>
2024-06-30 21:12:18 -0600

    Actualiza el modelo de usuario

--- src/main/java/com/alura/forohub/domain/usuario/UsuarioRepository.java
+++ src/main/java/com/alura/forohub/domain/usuario/UsuarioRepository.java
@@ -4,6 +4,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +12,6 @@ import java.util.Optional;
 @Repository
 public interface UsuarioRepository extends JpaRepository <Usuario, Long> {
     Page<Usuario> findByActivoTrue(Pageable paginacion);
+
+    UserDetails findByCorreoElectronico(String correoElectronico);
 }
