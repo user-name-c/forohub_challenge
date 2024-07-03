4ad53e51e9fba759c5bcfd5a4a0e2445fb67fc94
user-name-c <ivandgr_16@hotmail.com>
2024-07-03 11:14:37 -0600

    Crea repositorio de Curso con implementación de JPA

---
+++ src/main/java/com/alura/forohub/domain/curso/CursoRepository.java
@@ -0,0 +1,18 @@
+package com.alura.forohub.domain.curso;
+
+
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CursoRepository extends JpaRepository <Curso, Long> {
+    Page<Curso> findByActivoTrue(Pageable paginacion);
+
+    Curso findByNombre(String nombre);
+}
