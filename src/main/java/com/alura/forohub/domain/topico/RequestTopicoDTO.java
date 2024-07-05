f33417acb87c0e6649c8fe138b634ca6bb7d7519
user-name-c <ivandgr_16@hotmail.com>
2024-07-05 10:53:35 -0600

    No permite campos nulos al registrar un topico

--- src/main/java/com/alura/forohub/domain/topico/RequestTopicoDTO.java
+++ src/main/java/com/alura/forohub/domain/topico/RequestTopicoDTO.java
@@ -1,11 +1,15 @@
 package com.alura.forohub.domain.topico;
 
-import java.time.LocalDateTime;
+import jakarta.validation.constraints.NotNull;
 
 public record RequestTopicoDTO(
+        @NotNull
         String titulo,
+        @NotNull
         String mensaje,
+        @NotNull
         Long usuarioId,
+        @NotNull
         String curso
 ) {
 
