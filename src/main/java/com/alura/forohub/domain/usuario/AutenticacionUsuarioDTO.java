62af85f1bbe9d5d98aeb0a65ef653930d6e2e284
user-name-c <ivandgr_16@hotmail.com>
2024-10-19 14:40:56 -0600

    feat: add validation to authentication DTO fields
    Added @NotNull for both email and password, and @Email validation to ensure the correct format of the email field.

--- src/main/java/com/alura/forohub/domain/usuario/AutenticacionUsuarioDTO.java
+++ src/main/java/com/alura/forohub/domain/usuario/AutenticacionUsuarioDTO.java
@@ -1,4 +1,14 @@
 package com.alura.forohub.domain.usuario;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record AutenticacionUsuarioDTO(String email, String contrasena) {
+public record AutenticacionUsuarioDTO(
+        @NotNull
+        @Email
+        String email,
+
+        @NotBlank
+        String contrasena
+) {
 }
--- src/main/java/com/alura/forohub/domain/usuario/RegistroUsuarioDTO.java
+++ src/main/java/com/alura/forohub/domain/usuario/RegistroUsuarioDTO.java
@@ -1,13 +1,16 @@
 package com.alura.forohub.domain.usuario;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Email;
 
 public record RegistroUsuarioDTO(
         @NotNull
         String nombre,
         @NotNull
+        @Email
         String correoElectronico,
-        @NotNull
+        @NotBlank
         String contrasena
 ) {
 }
